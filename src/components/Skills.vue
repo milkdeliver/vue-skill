74a0e019c1063eb484947bfb4ee74f4141574c86
Milkdelivery <sam912358@gmail.com>
2018-07-08 23:42:06 +0800

    first step use vue

---
+++ src/components/Skills.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="hello">
+    Skills
+  </div>
+  
+</template>
+
+
+<script>
+  export default {
+    name: 'Skills',
+    data(){
+      return {
+        name: 'Sam Sun'
+      }
+    } 
+  }
+</script>
