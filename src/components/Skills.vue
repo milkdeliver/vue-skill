50dd2099a2c063f6c2f0d81e1c064be9ff868bda
Milkdelivery <sam912358@gmail.com>
2018-07-09 01:10:51 +0800

    use v-model and add method to input skill

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="hello">
     <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a skill..." v-model="skill"/>
+      </form>
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
+        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
       </ul>
 
-      <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }"></div>
     </div>
   </div>
   
@@ -16,31 +18,59 @@
     name: 'Skills',
     data(){
       return {
+        skill: '',
         skills : [
           { "skill": "vue.js"},
           { "skill": "Frontend Developer"}
         ],
-        showObject:{
-          alert: true,
-          'another-class': true
-        },
-        showAlert: true,
-        showAnotherClass: true,
-        bgColor: 'yellow',
-        bgWidth: '100%',
-        bgHeight: '30px'
+
       }
-    } 
+    },
+    methods: {
+      addSkill() {
+        this.skills.push({skill: this.skill})
+        this.skill = '';
+      }
+    }
   }
 </script>
 
 <style scoped>
-  .alert {
-    background-color: yellow;
-    width: 100%;
-    height:30px;
+  .holder {
+    background: #fff;
   }
-  .another-class {
-    border: 5px solid black;
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
   }
 </style>
