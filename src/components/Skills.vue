2ff213a5068426616262a41d2d0afbba2e1a0f21
Milkdelivery <sam912358@gmail.com>
2018-07-09 00:01:35 +0800

    use directive for button

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="hello">
-    Skills
+    {{ name }}
+
+    {{ btnStatus ? 'The button is disabled' : 'The button is active' }}
+    <button v-on:click="changeName" v-bind:disabled="btnStatus">Change Name</button>  
   </div>
   
 </template>
-
 
 <script>
   export default {
     name: 'Skills',
     data(){
       return {
+        btnStatus : true,
         name: 'Sam Sun'
       }
     } 
