1546c8e8bed8eedaefa328776033546e77d6ecd8
Milkdelivery <sam912358@gmail.com>
2018-07-17 19:28:16 +0800

    use route params

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="about">
-    <h1>Hello this is an about page</h1>
+    <h1>Hello, {{ their_name }} this is an about page</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
   </div>
-</templateu>
+</template>
+
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
