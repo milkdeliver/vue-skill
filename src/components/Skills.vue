13e98037b5be162f662ff3d6a79e4f61104b30ae
Milkdelivery <sam912358@gmail.com>
2018-07-17 18:32:19 +0800

    add animation for popup and group items

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,11 +3,17 @@
     <div class="holder">
       <form @submit.prevent="addSkill">
         <input type="text" placeholder="Enter a skill..." v-model="skill" v-validate="'min:5'" name="skill"/>
-        <!-- <transition name="alert-in "> -->
-        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        <transition name="alert-in"  enter-active-class="animated flipInX" leave-active-class="animated flipOutX"> 
+          <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        </transition>
       </form>
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+         <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+          <li v-for="(data, index) in skills" :key='index'>
+            {{ data.skill }}
+            <!-- <i class="fa fa-minus-circle" v-on:click="remove(index)"></i> -->
+          </li>
+        </transition-group>
       </ul>
 
     </div>
@@ -44,6 +50,9 @@
 </script>
 
 <style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"; 
+
   .holder {
     background: #fff;
   }
@@ -88,7 +97,26 @@
     font-weight:bold;
     padding: 5px;
     display: inline-block;
-    m5argin-top: -20px;
+    margin-top: -20px;
 
+  }
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 </style>
